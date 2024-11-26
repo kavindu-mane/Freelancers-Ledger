357e3c67593799a6d0e0ce8ab212fd8e45d3dd34
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2024-11-26 16:19:59 +0530

    setup DB connection and test models

--- API/Program.cs
+++ API/Program.cs
@@ -1,44 +1,41 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using NeonEfExample.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Load .env file
+DotNetEnv.Env.Load();
+
+// Access environment variables
+var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DBCon");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new Exception(
+        "Connection string not found. Ensure the .env file is correctly configured and placed in the root directory."
+    );
+}
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseAuthorization();
+app.MapControllers();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
+else
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    app.UseHttpsRedirection();
+}
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Freelancers-Ledger
  
+DB Migration
+```bash
+    dotnet ef migrations add InitialCreate
+    dotnet ef database update
+```
